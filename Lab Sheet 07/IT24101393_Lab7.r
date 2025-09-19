591cbe466d753ee267c09893e112a79ea1f8d28d
it24101393 <it24101393@my.sliit.lk>
2025-09-19 19:04:24 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101393_Lab7.r
@@ -0,0 +1,15 @@
+setwd("/Users/aksharaparami/Desktop/IT24100661")
+getwd()
+
+#1
+punif(15, min=10, max=25, lower.tail = TRUE)
+
+#2
+pexp(2, rate = 0.333, lower.tail = TRUE)
+
+#3
+#(i)
+pnorm(130, mean=100, sd=15, lower.tail = TRUE)
+
+#(ii)
+qnorm(0.95, mean=100, sd=15, lower.tail = TRUE)
